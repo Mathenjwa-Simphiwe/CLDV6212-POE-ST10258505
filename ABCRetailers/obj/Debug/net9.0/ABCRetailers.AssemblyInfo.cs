8f99d439c1b0e811a61cbf814a5d9735cb32d15d
Mathenjwa-Simphiwe <ST10258505@imconnect.edu.za>
2025-09-14 21:52:11 +0200

    commit

--- ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
+++ ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e84eb502167dcd4b2d156b9c608b70140bd07a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
